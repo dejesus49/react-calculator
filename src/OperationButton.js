bbbb8a870da42c00c5fd20ada33160d4b6b3fb02
dejesus49 <dejesus.49@osu.edu>
2023-08-11 21:56:34 -0400

    Prettier formatting

--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,5 +1,16 @@
-import { ACTIONS } from "./App"
+import { ACTIONS } from "./App";
 
-export default function DigitButton({dispatch, operation}) {
-    return <button onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: { operation}})}>{operation}</button>
+export default function DigitButton({ dispatch, operation }) {
+	return (
+		<button
+			onClick={() =>
+				dispatch({
+					type: ACTIONS.CHOOSE_OPERATION,
+					payload: { operation },
+				})
+			}
+		>
+			{operation}
+		</button>
+	);
 }
